12410577624640b80d1a18237d763cb0851e5b28
david <david@qq.com>
2020-01-09 09:52:11 +0800

    添加重算取消编辑奖期

--- database/migrations/2019_12_17_093227_create_order_transaction_logs_table.php
+++ database/migrations/2019_12_17_093227_create_order_transaction_logs_table.php
@@ -13,6 +13,7 @@ class CreateOrderTransactionLogsTable extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('lottery_categories')) return;
         Schema::create('order_transaction_logs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('user_id')->comment('用户id');
